fcfb0fca835c2693a99470c78c7e9305b4909886
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-09-04 19:16:55 +0200

    Repair Style calc

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -3,11 +3,10 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 const calculateStylePoints = (styleNotes) => {
   let min = Math.min.apply(null, styleNotes);
   let max = Math.max.apply(null, styleNotes);
-  var filteredStyleNotes = styleNotes.filter((e) => {return e != min && e != max})
-  var resultValue = filteredStyleNotes.reduce(reducer);
+  var resultValue = styleNotes.reduce(reducer);
   
   if (!isNaN(resultValue) && resultValue > 0 && styleNotes.length == 5 && resultValue <= 100){
-  	return resultValue 
+  	return resultValue - min - max
   } else {
   	return 'Error';
   }
